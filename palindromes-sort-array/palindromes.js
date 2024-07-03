ba0264335e312e2ed414f1d0a54f83ed2c2b14f2
Ennie <enniegd@gmail.com>
2024-07-03 01:03:28 +0100

    add comments

--- palindromes-sort-array/palindromes.js
+++ palindromes-sort-array/palindromes.js
@@ -1,13 +1,23 @@
+// Boolean funcs to reverse word
 function isPalindrome(word) {
   const revText = word.split("").reverse().join("");
+
+  // compare reverse word to word with strict equality.
   return revText === word;
 }
 
 function sortTexts(texts) {
+  // Create array for palindromes and normal word lists
   let palindromes = [];
   let normalWords = [];
 
+  // forEach loop: loops through list of text
   texts.forEach((word) => {
+    /*
+     * if statement: calls isPalindrome boolean func to check
+     * if word isPalindrome push word to palindromes array
+     * else push word to normalWord array
+     * */
     if (isPalindrome(word)) {
       palindromes.push(word);
     } else {
@@ -15,9 +25,11 @@ function sortTexts(texts) {
     }
   });
 
+  // return new lists as object of arrays
   return { palindrome: palindromes, normalWords: normalWords };
 }
 
+// main
 const texts = ["hello", "madam", "kayak", "text"];
 
 const result = sortTexts(texts);
